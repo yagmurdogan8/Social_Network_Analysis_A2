eaa3c2811e725d093195d046a91a5c56c1f98be1
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-15 23:25:28 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import networkx as nx
+import io
+import urllib.request
+
+# paring data from url
+small_data_url = urllib.request.urlopen("https://liacs.leidenuniv.nl/~takesfw/SNACS/twitter-small.tsv")
+large_data_url = urllib.request.urlopen("https://liacs.leidenuniv.nl/~takesfw/SNACS/twitter-larger.tsv")
+
+small_data = small_data_url.read().decode('utf-8')
+large_data_url = large_data_url.read().decode('utf-8')
