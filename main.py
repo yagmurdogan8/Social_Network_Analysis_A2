b06580b9f0dcc07b1280fdd9887747546f664d67
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-16 17:50:38 +0300

    parse data

--- main.py
+++ main.py
@@ -14,17 +14,16 @@ lines = small_data.split('\n')
 for line in lines:
     parts = line.strip().split('\t')
 
-    if len(parts) >= 3:
-        date, username, tweet = parts[0], parts[1], parts[2]
+    date, username, tweet = parts[0], parts[1], parts[2]
 
-        print(f"Date: {date}")
-        print(f"Username: {username}")
-        print(f"Tweet: {tweet}")
+    print(f"Date: {date}")
+    print(f"Username: {username}")
+    print(f"Tweet: {tweet}")
 
-        mentioned_users_list = {}
-
-        # for users in username:
-        #     for mention in tweet:
-        #         if mention.startswith('@'):
-        #             mention = mention[1:]  # for getting only the username without at
-        #             mentioned_users_list.keys().add(mention)
+    # mentioned_users_list = {}
+    #
+    # for users in username:
+    #     for mention in tweet:
+    #         if mention.startswith('@'):
+    #             mention = mention[1:]  # for getting only the username without at
+    #             mentioned_users_list.keys().add(mention)
