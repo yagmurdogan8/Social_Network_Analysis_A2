3f7881db116ddffc9ec6213331d621d4b7d8c6af
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-16 20:00:18 +0300

    parse data

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import urllib.request
 small_data_url = urllib.request.urlopen("https://liacs.leidenuniv.nl/~takesfw/SNACS/twitter-small.tsv")
 large_data_url = urllib.request.urlopen("https://liacs.leidenuniv.nl/~takesfw/SNACS/twitter-larger.tsv")
 
-small_data = small_data_url.read().decode('utf-8')  #str
+small_data = small_data_url.read().decode('utf-8')  # str
 large_data = large_data_url.read().decode('utf-8')
 
 lines = small_data.split('\n')
+mentioned_users_list = {}
 
-for index, line in enumerate(lines):
+for index, line in enumerate(lines):  # burada index almada sorun yasadim enum eklemek zorunda kaldim.
 
     parts = line.strip().split('\t')
     if len(parts) >= 3:
@@ -22,15 +23,24 @@ for index, line in enumerate(lines):
         print("Date: ", date)
         print("Username: ", username)
         print("Tweet: ", tweet)
+
+        words = tweet.split()
+        for word in words:
+            if word.startswith("@"):
+                mentioned_users = word[1:]
+                # print(mentioned_users)
+
+                if username in mentioned_users_list:
+                    mentioned_users_list[username].extend(mentioned_users)
+
+                else:
+                    mentioned_users_list[username] = []
+
+        for username, mentions in mentioned_users_list.items():
+            print(username, " mentioned: ", mentioned_users)
+
         print("-----------------------------------------------------")
+
     else:
         print("Tweet does not have tab spaces that I can split!!!")  # last line has a new line after it that's why i
         # added this
-
-    # mentioned_users_list = {}
-    #
-    # for users in username:
-    #     for mention in tweet:
-    #         if mention.startswith('@'):
-    #             mention = mention[1:]  # for getting only the username without at
-    #             mentioned_users_list.keys().add(mention)
